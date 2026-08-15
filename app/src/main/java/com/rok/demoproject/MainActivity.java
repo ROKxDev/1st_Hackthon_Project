27ae262187d567eb07fa8685cfd1a8388a7ac2ba
Sarthak Pande <sarthakpande21@gmail.com>
2026-08-15 10:45:45 +0530

    login page created

--- app/src/main/java/com/rok/demoproject/MainActivity.java
+++ app/src/main/java/com/rok/demoproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rok.demoproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,10 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Intent i = new Intent(MainActivity.this, LoginPage_Activity.class);
+        startActivity(i);
     }
 }
