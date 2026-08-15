0536278b612afc454c49d1fbfafdd71b28b364a6
Giri Shubham <shubhamgiri94274@gmail.com>
2026-08-15 18:08:07 +0530

    Aboutus Activity Backend

--- app/src/main/java/com/rok/demoproject/AboutUs_Activity.java
+++ app/src/main/java/com/rok/demoproject/AboutUs_Activity.java
@@ -1,6 +1,9 @@
 package com.rok.demoproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.rok.demoproject.Fragment.HomeFragment;
+
 public class AboutUs_Activity extends AppCompatActivity {
+
+    TextView btnBack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_about_us);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnBack=findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(AboutUs_Activity.this, HomeFragment.class);
+                startActivity(i);
+            }
         });
+
+
     }
 }
